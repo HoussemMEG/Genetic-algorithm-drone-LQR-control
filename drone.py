beac890103dac7bae3e64ae1f9474332b201416e
HoussemMEG <houssem.meghnoudj@g.enp.edu.dz>
2020-01-31 20:58:25 +0100

    last commit

--- drone.py
+++ drone.py
@@ -1,4 +1,4 @@
-from numpy import tan, sin, cos, deg2rad, rad2deg
+from numpy import tan, sin, cos, rad2deg
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -18,10 +18,10 @@ final_time = 5
 dt = 0.01
 time = np.arange(init_time, final_time + dt, dt)
 
+
 class Drone:
-    # % X = [phi theta psi P Q R Z Z_dot];
     def __init__(self, init_state=[0]*8, ref_state=[0]*8):
-        # state = [phi_0, theta_0, psi_0, P_0, Q_0, R_0, Z_0, Z_dot_0]
+        # state = [phi, theta, psi, P, Q, R, Z, Z_dot]
         self.init_state = init_state
         self.ref_state = ref_state
         self.state = init_state
@@ -29,8 +29,7 @@ class Drone:
         self.control_memory = [[0]*4]
 
     def step(self, U):
-        # this part is made to to use the values of state(t) to update state(t+1)
-        # at the same time
+        # Updating the new_state using the old state (not using the new calculations)
         new_state = [0]*8
         new_state[0] = self.state[0] + dt * (self.state[3] + tan(self.state[1]) *
                                              (sin(self.state[0])*self.state[4] + cos(self.state[0])*self.state[5]))
@@ -39,42 +38,37 @@ class Drone:
                                              cos(self.state[0])/cos(self.state[1])*self.state[5])
         new_state[3] = self.state[3] + dt * ((Iy-Iz)/Ix*self.state[4]*self.state[5] + U[0]/Ix
                                              - Kf_ax/Ix*(self.state[3])**2)
-        new_state[4] = self.state[4] + dt * ((Iz-Ix)/Iy*self.state[3]*self.state[5]+ U[1]/Iy
+        new_state[4] = self.state[4] + dt * ((Iz-Ix)/Iy*self.state[3]*self.state[5] + U[1]/Iy
                                              - Kf_ay/Iy*(self.state[4])**2)
         new_state[5] = self.state[5] + dt * ((Ix-Iy)/Iz*self.state[3]*self.state[4] + U[2]/Iz
                                              - Kf_az/Iz*(self.state[5])**2)
         new_state[6] = self.state[6] + dt * (self.state[7]) if ((self.state[6] + dt * (self.state[7])) <= 0) else 0
         new_state[7] = self.state[7] + dt * (g - U[3]/m*cos(self.state[0])*cos(self.state[1]))
         self.state = new_state
-        # saving the new state and control signals
+        # saving the new state and control signals in the memory in order to compute the fitness value
         self.state_memory.append(new_state)
         self.control_memory.append(U)
         return new_state
 
     def control(self, K):
-        # K = [[0.0151, 0, 0, 0.0545, 0, 0, 0, 0],
-        #      [0, 0, 0, 0, 0.0767, 0, 0, 0],
-        #      [0, 0, 1.8898, 0, 0, 0.1991, 0, 0],
-        #      [0, 0, 0, 0, 0, 0, -97.1825, -15.2758]]
-
-        # K.append(K_end)
         K = np.array(K)
+        # State feedback with a small change made
         control = -K.dot(np.array(self.state)-self.ref_state) + \
                   [0, 0, 0, m*g/(cos(self.state[0])*cos(self.state[1]))]
+        # Permitted control values
         control[0] = max(-1, min(control[0], 1))
         control[1] = max(-1, min(control[1], 1))
         control[2] = max(-1, min(control[2], 1))
         control[3] = max(0, min(control[3], 10))
-        #print('CONTROL :', control)
         return control
 
-    def simulate(self, K_end):
-        for i in range(len(time)-1):
-            self.step(self.control(K_end))
+    def simulate(self, K):
+        for i in range(len(time) - 1):
+            self.step(self.control(K))
 
     def position_show(self):
         plt.figure()
-        plt.title("Z position simulation")
+        plt.title("z position simulation")
         ax = plt.axes()
         plt.grid(b=True)
         ax.plot(time, [row[6] for row in self.state_memory], label='Z')
@@ -88,20 +82,29 @@ class Drone:
         plt.title("Angles simulation")
         plt.grid(b=True)
         plt.plot(time, [rad2deg(row[0]) for row in self.state_memory], label="Phi")
-        plt.plot(time, [rad2deg(row[2]) for row in self.state_memory], label="Theta")
-        plt.plot(time, [rad2deg(row[4]) for row in self.state_memory], label="Psi")
+        plt.plot(time, [rad2deg(row[1]) for row in self.state_memory], label="Theta")
+        plt.plot(time, [rad2deg(row[2]) for row in self.state_memory], label="Psi")
         plt.legend(loc='upper right')
         ax.set(xlabel='Temps [s]', ylabel='Angle [deg]')
         plt.show()
 
-    def control_show(self):
+    def control_angle_show(self):
         plt.figure()
         ax = plt.axes()
-        plt.title("Control signals simulation")
+        plt.title("Control signals (angles) simulation")
         plt.grid(b=True)
         plt.plot(time, [row[0] for row in self.control_memory], label="Tau_phi")
         plt.plot(time, [row[1] for row in self.control_memory], label="Tau_theta")
         plt.plot(time, [row[2] for row in self.control_memory], label="Tau_psi")
+        plt.legend(loc='upper right')
+        ax.set(xlabel='Temps [s]', ylabel='Amplitude [SI]')
+        plt.show()
+
+    def control_thrust_show(self):
+        plt.figure()
+        ax = plt.axes()
+        plt.title("Control signals (position) simulation")
+        plt.grid(b=True)
         plt.plot(time, [row[3] for row in self.control_memory], label="Thrust")
         plt.legend(loc='upper right')
         ax.set(xlabel='Temps [s]', ylabel='Amplitude [SI]')
@@ -111,7 +114,3 @@ class Drone:
         self.state_memory = [self.init_state]
         self.control_memory = [[0]*4]
         self.state = self.init_state
-
-
-
-#[-0.1992, 0.0156, -0.1297, -0.0100, 0.0009, -0.0065, -97.2003, -14.5800]
--- main.py
+++ main.py
@@ -1,48 +1,48 @@
 from drone import Drone
 from genetic_algorithm import *
-from tqdm import tqdm
-from utils import *
-import numpy as np
+from math import pi
 
-# Initialisation
-population_number = 20
 
+def deg2rad(list1):
+    list1[0] = list1[0] / 180 * pi
+    list1[1] = list1[1] / 180 * pi
+    list1[2] = list1[2] / 180 * pi
+    return list1
+
+
+# Initialisation and algorithm object creation
+steps = 200
+population_number = 100
 ga = GA(population_number)
 
-# Creating a drone
-init_state = deg2rad([0, 0, 1, 0, 0, 0, -1, 0])
+# Creating, initialising and giving to the drone the reference
+# The angles are in degrees, and : state = [phi, theta, psi, P, Q, R, Z, Z_dot]
+init_state = deg2rad([5, 5, 5, 0, 0, 0, -1, 0])
 ref_state = deg2rad([0, 0, 0, 0, 0, 0, 0, 0])
 drone = Drone(init_state=init_state, ref_state=ref_state)
-# un resultat [-0.1992, 0.0156, -0.1297, -0.01, 0.0009, -0.0065, -95.43, -17.12]
 
-number = 10
-for hello in range(number):
+for generation in range(steps):
     all_state_memories = []
     all_control_memories = []
+    if generation % 10 == 0:
+        print("Generation :", generation)
     for index in range(population_number):
+        # Resetting the drone memory
+        drone.reset_memory()
+        # Simulating the drone for that gain
         drone.simulate(ga.population[index])
+        # Storing the memory for the fitness calculation
         all_state_memories.append(drone.state_memory)
         all_control_memories.append(drone.control_memory)
-        if (index == population_number - 1) and hello == number-1:
-            print('\ngain :', ga.population[index])
-            drone.position_show()
-            drone.angle_show()
-            drone.control_show()
-        drone.reset_memory()
+    # Selecting individual
     selection = ga.selection(all_state_memories, all_control_memories)
+    # Mating and mutation
     ga.mating(selection)
     ga.mutation()
-
-        #drone.angle_show()
-        #drone.position_show()
-        #drone.control_show()
-
-#drone.position_show()
-
-#ga.calculate_fitness(all_state_memories, all_control_memories)
-#print(ga.mating(ga.selection(all_state_memories, all_control_memories)))
-#ga.mutation()
-#print(ga.population)
-
-
-# problème qui peut sortir c'est scale entre les z et U dans le GA
+    # Printing and showing graphs part
+    if generation == steps - 1:
+        print('\ngain :', ga.population[index])
+        drone.position_show()
+        drone.angle_show()
+        drone.control_angle_show()
+        drone.control_thrust_show()
